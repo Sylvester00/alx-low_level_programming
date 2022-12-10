ff820abffae7257ca86ae8df2d881e58e1a2717d
Sylvester00 <sylvesterhotahua001@gmail.com>
2022-12-10 06:43:18 -0800

    All

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,27 +2,7 @@
 
 #include <unistd.h>
 
-/**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
 
-int main(void)
-
-{
-
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
-	return (1);
-
-}#include <stdio.h>
-
-#include <unistd.h>
 
 /**
  *
@@ -38,8 +18,8 @@ int main(void)
 
 {
 
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+		write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (1);
+			return (1);
 
 }
