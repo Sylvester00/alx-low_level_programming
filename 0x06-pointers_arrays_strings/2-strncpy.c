d55122bb50ad94e80d034cffd5ea6ae61440855f
Sylvester00 <sylvesterhotahua001@gmail.com>
2022-12-22 00:15:10 -0800

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,10 +1,10 @@
-i#include "main.h"
+#include "main.h"
 
 /**
- * _strncpy - copy a string
- * @dest: input value
- * @src: input value
- * @n: input value
+ * _strncpy - copy a string with n
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
  * Return: dest
  */
 char *_strncpy(char *dest, char *src, int n)
